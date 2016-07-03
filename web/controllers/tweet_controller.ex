3be094f1b047035bd482ad2a6601e57e33540d5e
Franziska Schmidt <franziska.petra@gmail.com>
2016-07-03 17:49:26 +0200

    It deletes things

--- web/controllers/tweet_controller.ex
+++ web/controllers/tweet_controller.ex
@@ -1,7 +1,23 @@
 defmodule DeleteYourTweets.TweetController do
   use DeleteYourTweets.Web, :controller
 
-  def delete(conn, _params) do
+  def delete(conn, %{"older_than_months" => older_than_months}) do
 
+    older_than_months
+    |> fetch_tweets
+    |> delete_tweets
+
+    redirect conn, to: "/"
+  end
+
+  defp fetch_tweets(number_months) do
+    ExTwitter.user_timeline(count: 3)
+  end
+
+  defp delete_tweets(tweets) do
+    Enum.map(tweets,
+      fn tweet ->
+        ExTwitter.destroy_status(tweet.id)
+      end)
   end
 end
