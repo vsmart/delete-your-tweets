132d8cb7cd20e6e1bcc7523392709e4180bade94
Franziska Schmidt <franziska.petra@gmail.com>
2016-08-09 22:46:01 +0200

    Update tests to remove ecto

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,1 @@
 ExUnit.start
-
-Mix.Task.run "ecto.create", ~w(-r DeleteYourTweets.Repo --quiet)
-Mix.Task.run "ecto.migrate", ~w(-r DeleteYourTweets.Repo --quiet)
-Ecto.Adapters.SQL.begin_test_transaction(DeleteYourTweets.Repo)
-
