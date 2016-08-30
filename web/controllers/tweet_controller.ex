84bdde6993f199fa5e1b623db3575edea49f64f5
Franziska Schmidt <franziska.petra@gmail.com>
2016-08-30 21:16:30 +0200

    It kinda works

--- web/controllers/tweet_controller.ex
+++ web/controllers/tweet_controller.ex
@@ -2,16 +2,28 @@ defmodule DeleteYourTweets.TweetController do
   use DeleteYourTweets.Web, :controller
 
   def delete(conn, %{"older_than_months" => older_than_months}) do
-    max_id = get_latest_tweet_id
+    latest_tweet_id = get_latest_tweet_id
+    fetch_and_delete_from(latest_tweet_id)
 
     conn
-    |> put_flash(:info, "last tweet id is #{max_id}. 🎉")
+    |> put_flash(:info, "Deleted a bunch of your tweets. Wheee 🎉")
     |> redirect(to: "/")
   end
 
   defp get_latest_tweet_id  do
     [%ExTwitter.Model.Tweet{id: id}] = ExTwitter.user_timeline(count: 1)
     id
+  end
+
+  defp fetch_and_delete_from(max_id) do
+    tweets = fetch_tweets(max_id)
+    IO.puts "Fetched #{Enum.count(tweets)} tweets."
+    delete_tweets(tweets)
+
+    if Enum.count(tweets) > 0 do
+      %ExTwitter.Model.Tweet{id: last_tweet_id} = List.last(tweets)
+      fetch_and_delete_from(last_tweet_id)
+    end
   end
 
   defp fetch_tweets(max_id) do
