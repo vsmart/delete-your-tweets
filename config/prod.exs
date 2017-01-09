e5f8b57ed406b3d43fa1a885d34d7d663bde6c32
Franziska Schmidt <hello@franka.tech>
2017-01-09 14:00:56 +0100

    Add prod db config

--- config/prod.exs
+++ config/prod.exs
@@ -10,3 +10,10 @@ config :delete_your_tweets, DeleteYourTweets.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
      config :delete_your_tweets, DeleteYourTweets.Endpoint, root: "."
+
+# Configure your database
+config :delete_your_tweets, DeleteYourTweets.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
