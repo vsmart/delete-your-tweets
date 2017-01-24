c6238454f431f93f42974c90e7fae70be39e2036
Franziska Schmidt <hello@franka.tech>
2017-01-25 00:00:19 +0100

    use http

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :delete_your_tweets, DeleteYourTweets.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "delete-your-tweets.franka.tech", port: 443],
+  url: [scheme: "http", host: "delete-your-tweets.franka.tech", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
