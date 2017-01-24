aeeaf1ced6d66cac41a778c01802c051df89c291
Franziska Schmidt <hello@franka.tech>
2017-01-24 20:34:04 +0100

    use HEROKU_URL as host

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :delete_your_tweets, DeleteYourTweets.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "delete-your-tweets.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("HEROKU_URL"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
