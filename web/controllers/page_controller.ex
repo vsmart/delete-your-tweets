1259dc7e7a79fd79d6cba8326655cf0a1b794efe
Franziska Schmidt <hello@franka.tech>
2017-05-08 21:12:29 +0200

    Add about page with stats

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,10 +3,17 @@ defmodule DeleteYourTweets.PageController do
 
   def index(conn, _params) do
     DeleteYourTweets.configure_twitter_client
+
+    conn
+    |> delete_session(:screen_name)
+    |> render("index.html", step: :one)
+  end
+
+  def about(conn, _params) do
     deletion_count = Repo.one(from td in DeleteYourTweets.TweetDeletion, select: count("*"))
 
     conn
     |> delete_session(:screen_name)
-    |> render("index.html", step: :one, deletion_count: deletion_count)
+    |> render("about.html", deletion_count: deletion_count)
   end
 end
